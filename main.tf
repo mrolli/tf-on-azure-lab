c82f7cd14fbdf64e81ab1aa3a8bdde27c9ef9695
Michael Rolli <michael@rollis.ch>
2026-06-25 14:12:54 +0200

    feat: add tags to rg

--- main.tf
+++ main.tf
@@ -1,20 +1,26 @@
 # Specifiy the provider and version
- terraform {
-   required_providers {
-     azurerm = {
-       source  = "hashicorp/azurerm"
-       version = "~> 4.0"
-     }
-   }
- }
+terraform {
+  required_version = ">= 1.15.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
 
- # Configure the Microsoft Azure Provider
- provider "azurerm" {
-   features {}
- }
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
 
- # Create the very first resource
- resource "azurerm_resource_group" "this" {
-   name     = "rg-lab-mr-test"
-   location = "Switzerland North"
- }
+# Create the very first resource
+resource "azurerm_resource_group" "this" {
+  name     = "rg-lab-mr-test"
+  location = "switzerlandnorth"
+
+  tags = {
+    Environment = "test"
+    ManagedBy   = "terraform"
+  }
+}
